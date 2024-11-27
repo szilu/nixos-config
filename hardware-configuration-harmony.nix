127e690dbd6a571a09d067da130e5a75d50ca2b5
Szilárd Hajba <szilu@symbion.hu>
2024-11-27 12:42:24 +0100

    Added swapfile

--- hardware-configuration-harmony.nix
+++ hardware-configuration-harmony.nix
@@ -35,7 +35,7 @@
 			};
 	};
 
-	swapDevices = [ ];
+	swapDevices = [{ device = "/var/lib/swap"; size = 16 * 1024; }];
 
 	# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
 	# (the default) this is the recommended approach. When using systemd-networkd it's
