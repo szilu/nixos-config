556c322a48530d0d0aa4b3eb3c0b0188ec4e48c0
Szilárd Hajba <szilu@symbion.hu>
2026-06-23 18:37:48 +0200

    chore(flake): remove commented legacy unstable-overlay outputs

--- flake.nix
+++ flake.nix
@@ -30,41 +30,4 @@
 		};
 }
 
-#{
-#	description = "NixOS configuration";
-#
-#	inputs = {
-#		nixpkgs.url = "nixpkgs/nixos-24.11";
-#		nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-#	};
-#
-#	outputs = { self, nixpkgs, nixpkgs-unstable }:
-#		let
-#			system = "x86_64-linux";
-#			overlay-unstable = final: prev: {
-#				#unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-#				unstable = import nixpkgs-unstable {
-#					 inherit system;
-#					 config.allowUnfree = true;
-#				};
-#
-#			};
-#		in {
-#			nixosConfigurations.fanny = nixpkgs.lib.nixosSystem {
-#				inherit system;
-#				modules = [
-#					({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
-#					./configuration-fanny.nix
-#				];
-#			};
-#			nixosConfigurations.harmony = nixpkgs.lib.nixosSystem {
-#				inherit system;
-#				modules = [
-#					({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable]; })
-#					./configuration-harmony.nix
-#				];
-#			};
-#		};
-#}
-
 # vim: ts=4
