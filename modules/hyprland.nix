c738e4c7447726b5de42d51ca9ec53b14d51098e
Szilárd Hajba <szilu@symbion.hu>
2026-06-23 18:53:24 +0200

    feat(hyprland): mask tuigreet password with asterisks

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -42,7 +42,7 @@ in
 	services.greetd = {
 		enable = true;
 		settings.default_session = {
-			command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd ${config.programs.hyprland.package}/bin/start-hyprland";
+			command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd ${config.programs.hyprland.package}/bin/start-hyprland";
 			user = "greeter";
 		};
 	};
